6a0436770c0009d60f28025c34b78c27da31afd7
LittleKenzy <keygaming589@gmail.com>
2025-09-10 20:23:18 +0700

    sigma

--- main.js
+++ main.js
@@ -6,13 +6,32 @@ console.log(Token)
 
 const bot = new TelegramBot(Token, { polling: true });
 
-bot.on('message', (data) => {
-    if (data.text == '/start') {
-        return bot.sendMessage(data.from.id, `Welcome, ${data.from.first_name}! How can I assist you today?`);
-    } else {
-        bot.sendMessage(data.from.id, `Hello, ${data.from.first_name}! You said: ${data.text}`);
-    }
+// bot.on('message', async (data) => {
+//     if (data.text !== '!start') {
+//         const botProfile = await bot.getMe()
+//         console.log(botProfile)
+//         bot.sendMessage(data.from.id, `Hello little kenzy ${data.from.first_name}, I am ${botProfile.first_name}. Ada yang bisa saya bantu?`);
+//     }
+// })
+
+// ini listener untuk message spesific sticker only
+bot.on('sticker', (data) => {
+    return bot.sendMessage(data.from.id, data.sticker.emoji);
 })
+
+
+// spesifik hanya ketika user ngetik !start
+bot.onText(/^!start$/, (msg) => {
+    bot.sendMessage(msg.from.id, `Hello, ${msg.from.first_name}! Welcome to the bot. Ada yang bisa saya bantu?`);
+})
+
+// ketika user ngetik !follow diikuti dengan pesan
+bot.onText(/^!follow(.+)/, (msg, after) => {
+    console.log(after[1])
+    bot.sendMessage(msg.from.id, `Thank you for following me, ${msg.from.first_name}! You said: ${after[1]}`);
+})
+
+
 
 // bot.on('message', (msg) => {
 //     const chatId = msg.chat.id;
